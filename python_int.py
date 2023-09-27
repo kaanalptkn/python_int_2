9356b376f6620f68ec7883f15901eb55cb1f8077
Kaan Alptakin <92170029+kaanalptkn@users.noreply.github.com>
2023-09-27 13:23:40 +0100

    Created python file and solved task

---
+++ python_int.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    
+# Perform for integer and float divison    
+integer_result = a//b
+float_result = a/b
+
+
+# Print the result
+print(integer_result)
+print(float_result)
